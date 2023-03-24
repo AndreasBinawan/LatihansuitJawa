bba29fb34e6de970e729de3faceb108bc8c649dc
Andreas Binawan <82919256+AndreasBinawan@users.noreply.github.com>
2023-03-24 15:07:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,39 @@
 var tanya = true;
-while (tanya) {
+while( tanya ) {
+    // menangkap pilihan player
+    var p = prompt('pilih : gajah, semut, orang');
 
-    // Menangkap pilihan user
-    var p = prompt('Silahkan pilih: gajah, semut, orang');
-
-
-    // Menangkap Pilihan Computer
-    // Membangkitkan bilangan random
+    // menangkap pilihan computer
+    // membangkitkan bilangan random
     var comp = Math.random();
 
-
-    if (comp < 0.34) {
+    if( comp < 0.34 ) {
         comp = 'gajah';
-    } else if (comp >= 0.34 && comp < 0.67) {
+    } else if( comp >= 0.34 && comp < 0.67 ) {
         comp = 'orang';
     } else {
         comp = 'semut';
     }
 
-    // Menentukan rules
+    // menentukan rules
     var hasil = '';
-
-    if (p == comp) {
-        hasil = 'SERI';
-    } else if (p == 'gajah') {
-        //if (comp == 'orang') {
-        //   hasil = 'MENANG';
-        //   } else {
-        //hasil = 'KALAH';
-        // }
-        hasil = (comp == 'orang') ? 'MENANG' : 'KALAH';
-    } else if (p == 'orang') {
-        hasil(comp == 'gajah') ? 'KALAH' : 'MENANG';
-    } else if (p == 'semut') {
-        hasil = (comp == 'orang') ? 'KALAH' : 'MENANG';
+    if( p == comp ) {
+        hasil = 'SERI!';
+    } else if( p == 'gajah' ) {
+        hasil = ( comp == 'orang' ) ? 'MENANG!' : 'KALAH!';
+    } else if( p == 'orang' ) {
+        hasil = ( comp == 'gajah' ) ? 'KALAH!' : 'MENANG!';
+    } else if( p == 'semut' ) {
+        hasil = ( comp == 'orang' ) ? 'KALAH' : 'MENANG!';
     } else {
-        hasil = 'Memasukan pilihan yang salah!';
+        hasil = 'memasukkan pilihan yang salah!';
     }
 
-    // Menampilkan hasil
-    alert('Kamu memilih : ' + p + ' dan komputer memilih: ' + comp + '\nMaka hasilnya : Kamu  ' + hasil);
+    // tampilkan hasilnya
+    alert('Kamu memilih : ' + p + ' dan Komputer memilih : ' + comp + '\nMaka hasilnya : Kamu ' + hasil);
 
-    tanya = confirm('Main lagi?');
+    tanya = confirm('lagi?');
 }
 
-alert('Terima kasih sudah bermain');
+alert('terimakasih sudah bermain.');
+
